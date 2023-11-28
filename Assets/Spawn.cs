07a31c922c81f2556d19485a7998aedf0606f785
wakim17 <johnywakim17@gmail.com>
2023-11-28 16:10:46 +0100

    misc

--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -5,7 +5,8 @@ public class Spawn : MonoBehaviour
     public GameObject pipePrefab;
     public float spawnInterval = 2f;
     public float pipeSpeed = 2f;
-    public float spawnHeightRange = 3f; // Adjust the range of random Y positions
+
+    public float[] spawnHeights = { 1f, 2f, 3f };
 
     void Start()
     {
@@ -14,27 +15,25 @@ public class Spawn : MonoBehaviour
 
     void SpawnPipes()
     {
-        // Generate random Y positions for both above and below the player
-        float randomY1 = Random.Range(-spawnHeightRange / 2f, spawnHeightRange / 2f);
-        float randomY2 = Random.Range(-spawnHeightRange / 2f, spawnHeightRange / 2f);
-
-        // Spawn pipes at the random positions
-        SpawnPipeAtPosition(randomY1);
-        SpawnPipeAtPosition(randomY2);
+    
+        float randomY = spawnHeights[Random.Range(0, spawnHeights.Length)];
+ 
+        SpawnPipeAtPosition(randomY);
+ 
+        float oppositeY = -randomY;
+ 
+        SpawnPipeAtPosition(oppositeY);
     }
 
     void SpawnPipeAtPosition(float yPosition)
     {
-        // Create a new position with the specified Y coordinate
+ 
         Vector3 spawnPosition = new Vector3(transform.position.x, yPosition, transform.position.z);
-
-        // Instantiate the pipe at the new position
+ 
         GameObject newPipe = Instantiate(pipePrefab, spawnPosition, Quaternion.identity);
-
-        // Set the parent to this transform for organization in the Unity Editor
+ 
         newPipe.transform.parent = transform;
-
-        // Access the Rigidbody2D component of the pipe and set its velocity to move left
+ 
         Rigidbody2D pipeRb = newPipe.GetComponent<Rigidbody2D>();
         pipeRb.velocity = new Vector2(-pipeSpeed, 0f);
     }
